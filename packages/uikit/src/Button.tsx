9077d07c6bebc6dd5dee79992ca57c07d945d7a4
egorkqq <egorkqq@gmail.com>
2024-09-04 18:40:28 +0300

    feat: delete uikit

--- packages/uikit/src/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { type ComponentProps } from "react";
-
-export const Button = (props: ComponentProps<"button">) => {
-  const { type = "button", ...rest } = props;
-
-  return <button className="bg-blue-400 p-5 text-zinc-400" type={type} {...rest} />;
-};
-
-export default Button;
