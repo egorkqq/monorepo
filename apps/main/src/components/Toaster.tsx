6ecc26680ee6e77f4b6d12b2e9b09c19c674db23
egorkqq <egorkqq@gmail.com>
2024-10-23 00:44:32 +0300

    fix: toasts in production

--- apps/main/src/components/Toaster.tsx
+++ apps/main/src/components/Toaster.tsx
@@ -1,14 +1,21 @@
 import type { DefaultToastOptions } from "react-hot-toast";
 
+import { memo } from "react";
 import { Toaster } from "react-hot-toast";
 
-const toastOptions: DefaultToastOptions = {
-  duration: 2000,
-  className: "bg-[#2D2D2E] rounded-2xl py-2 px-4 text-white font-medium text-headline shadow-elevation3",
-  error: {
-    duration: 5000,
-    className: "bg-accent rounded-2xl py-2 px-4 text-white font-medium text-headline shadow-elevation3",
-  },
-};
+import { cn } from "@arc/ui/cn";
 
-export const StyledToaster = () => <Toaster toastOptions={toastOptions} />;
+export const StyledToaster = memo(() => {
+  const toastOptions: DefaultToastOptions = {
+    duration: 2000,
+    className: cn("text-headline shadow-elevation3 rounded-2xl bg-[#2D2D2E] px-4 py-2 font-medium text-white"),
+    error: {
+      duration: 5000,
+      className: cn("bg-accent text-headline shadow-elevation3 rounded-2xl px-4 py-2 font-medium text-white"),
+    },
+  };
+
+  return <Toaster toastOptions={toastOptions} />;
+});
+
+StyledToaster.displayName = "StyledToaster";
