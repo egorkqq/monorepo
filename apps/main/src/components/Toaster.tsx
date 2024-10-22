c0e6139c521585c422ef7eb97bd5835e13f55da7
egorkqq <egorkqq@gmail.com>
2024-10-23 00:50:20 +0300

    fix: toasts in production

--- apps/main/src/components/Toaster.tsx
+++ apps/main/src/components/Toaster.tsx
@@ -8,10 +8,10 @@ import { cn } from "@arc/ui/cn";
 export const StyledToaster = memo(() => {
   const toastOptions: DefaultToastOptions = {
     duration: 2000,
-    className: cn("text-headline shadow-elevation3 rounded-2xl bg-[#2D2D2E] px-4 py-2 font-medium text-white"),
+    className: cn("!text-headline !shadow-elevation3 !rounded-2xl !bg-[#2D2D2E] !px-4 !py-2 !font-medium !text-white"),
     error: {
       duration: 5000,
-      className: cn("bg-accent text-headline shadow-elevation3 rounded-2xl px-4 py-2 font-medium text-white"),
+      className: cn("!bg-accent !text-headline !shadow-elevation3 !rounded-2xl !px-4 !py-2 !font-medium !text-white"),
     },
   };
 
